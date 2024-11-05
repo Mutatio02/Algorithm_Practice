0fb57711a4ac635fdd9ef9ab18185bd9d5fa3bc0
Mutatio02 <144579556+Mutatio02@users.noreply.github.com>
2024-11-05 17:31:52 +0900

    [Silver I] Title: 그림, Time: 16 ms, Memory: 3248 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -3,63 +3,54 @@
 #include <vector>
 #include <queue>
 using namespace std;
-
+// 2024.11.05 복습
+int p[501][501]; // 그림
+bool visit[501][501];
+int dx[4] = {0,1,0,-1};
+int dy[4] = {1,0,-1,0};
 
 int main(void) {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
-    int board[501][501] = {0};
-    bool visit[501][501] = {false};
-    int dx[4] = {0,-1,0,1};
-    int dy[4] = {1,0,-1,0};
-    int N,M;
-    cin >> N >> M;
 
-    int cnt = 0; // 그림 개수
-    int big = 0; // 가장 넓은 그림
+    int N,M;
+    cin >> N >> M; // 세로 가로
 
     for(int i=0; i<N; i++) {
         for(int j=0; j<M; j++) {
-            cin >> board[i][j];
-
+            cin >> p[i][j];
         }
     }
+    int cnt = 0; // 그림개수
+    int pic = 0; // 그림 넓이
+    queue<pair<int,int>> que;
 
     for(int i=0; i<N; i++) {
         for(int j=0; j<M; j++) {
-            if(board[i][j] == 0 || visit[i][j] == true) // 색칠이 안되어 있거나 이미 방문했다면
-                continue;
-
-            cnt++; // 그림의 개수 증가 
-            queue<pair<int,int>> que;
+            if(p[i][j] ==0 || visit[i][j]) continue;
+            cnt++;
             que.push({i,j});
-            visit[i][j] = true; // 방문 완료
-            int area = 0; // 가장 큰 면적을 찾을 변수
+            visit[i][j] = 1;
+            int area = 0;
             while(!que.empty()) {
-                area++;
-                pair<int,int> cur = que.front();
+                auto cur = que.front();
                 que.pop();
-                for(int dir = 0; dir <4; dir++) { // 상하좌우 탐색
-                    int nx = cur.first + dx[dir];
-                    int ny = cur.second + dy[dir];
+                area++;
 
-                    if(nx < 0 || nx >= N || ny < 0  || ny >= M) { // 탐색 범위가 넘어 갔으면
-                        continue; // 무시
-                    }
-                    if(board[nx][ny] == 0 || visit[nx][ny] == true) { // 상하좌우 탐색이 부분이 0이거나 이미 방문했다면
-                        continue; // 무시
-                    }
-                    
-                    visit[nx][ny] = true; // 나머지는 방문 했다고 표시
+                for(int j=0; j<4; j++) {
+                    int nx = cur.first + dx[j];
+                    int ny = cur.second + dy[j];
+
+                    if(nx<0|| nx >=N || ny<0 || ny>=M) continue;
+                    if(p[nx][ny] == 0 || visit[nx][ny] == 1) continue;
+
                     que.push({nx,ny});
+                    visit[nx][ny] = 1;
                 }
             }
-            big = max(big,area);
+            pic = max(area,pic);
         }
     }
-
-    cout << cnt << '\n' << big;
-
+    cout << cnt << "\n" << pic;
     return 0;
 }
