03f1cc70a96e21b4589277c04b2ec02983f84bd5
Mutatio02 <144579556+Mutatio02@users.noreply.github.com>
2024-11-09 15:52:27 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2200 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -2,42 +2,42 @@
 #include <algorithm>
 #include <vector>
 #include <queue>
+// 24.11.09 복습 -- 미로찾기
 using namespace std;
 
 int dx[4] = {0,1,0,-1};
 int dy[4] = {1,0,-1,0};
+string board[101];
+int visit[101][101];
 
-string board[101]; // 입력
-int visit[101][101]; // 미로
 int main(void) {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+
     int N,M;
     cin >> N >> M;
 
     for(int i=0; i<N; i++) {
         cin >> board[i];
-    } // 입력
+    }
+
     queue<pair<int,int>> que;
+    que.push({0,0});
     visit[0][0] = 1;
 
-    que.push({0,0});
     while(!que.empty()) {
         auto cur = que.front();
         que.pop();
+
         for(int i=0; i<4; i++) {
             int nx = cur.first + dx[i];
             int ny = cur.second + dy[i];
 
-            if(nx < 0 || nx >= N || ny < 0 || ny >= M) { // 범위를 넘어가면 초기화
-                continue;
-            }
-            if(board[nx][ny] == '0' || visit[nx][ny] != 0) {
-                continue;
-            }
-            visit[nx][ny] = visit[cur.first][cur.second] +1;
+            if(nx<0 || nx >= N || ny<0 || ny>=M)    continue; // 범위 지나면 무시
+            if(board[nx][ny] == '0'|| visit[nx][ny] != 0) continue;
+
             que.push({nx,ny});
+            visit[nx][ny] = visit[cur.first][cur.second] + 1;
         }
     }
     cout << visit[N-1][M-1];
