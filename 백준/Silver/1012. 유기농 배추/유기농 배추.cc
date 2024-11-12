a0f3bebc350030272bcd1ea0b8ed9756901b383e
Mutatio02 <144579556+Mutatio02@users.noreply.github.com>
2024-11-12 16:32:03 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -3,62 +3,61 @@
 #include <vector>
 #include <queue>
 using namespace std;
-int board[51][51]; // 밭
-bool visit[51][51]; // 방문여부
+// 24.11.12 -- 복습
+int board[51][51];
+bool visit[51][51];
 int dx[4] = {0,1,0,-1};
 int dy[4] = {1,0,-1,0};
+
 int main(void) {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+
     int T;
     cin >> T;
     while(T--) {
-        int M,N,K; // 가로 세로 위치 개수
+        int M,N,K;
         cin >> M >> N >> K;
-        // 초기화
+        queue<pair<int,int>> q;
+        int cnt = 0;
+
         for(int i=0; i<N; i++) {
             for(int j=0; j<M; j++) {
                 board[i][j] = 0;
-                visit[i][j] = false;
+                visit[i][j] =false;
             }
         }
-
         for(int i=0; i<K; i++) {
-            int x, y;
-            cin >> x >> y;
-            board[y][x] = 1; // 1은 배추가 있는 곳
+            int x,y;
+            cin >> x >> y; // x y 좌표
+            board[y][x] = 1; // 반대로
         }
-        queue<pair<int,int>> que;// 좌표를 담는 큐
-        int cnt = 0; // 배추 묶음
-        
+
         for(int i=0; i<N; i++) {
             for(int j=0; j<M; j++) {
-                if(board[i][j] == 1 && visit[i][j] == false) { // 배추가 있는데 방문하지 않은 경우
-                    cnt++;
-                    que.push({i,j});
-                    visit[i][j] = true;
+                if(board[i][j] != 1 || visit[i][j]) continue;
+                cnt++;
 
-                    while(!que.empty()) {
-                        auto cur = que.front();
-                        que.pop();
+                q.push({i,j});
+                visit[i][j]= true;
 
-                        for(int i = 0; i<4; i++) {
-                            int nx = cur.first + dx[i];
-                            int ny = cur.second + dy[i];
+                while(!q.empty()) {
+                    auto cur = q.front();
+                    q.pop();
 
-                            if(nx<0 || nx >= N || ny <0 || ny >=M)  continue; // 범위를 지나가면 무시
-
-                            if(board[nx][ny] == 1 && visit[nx][ny] == false) { // 주위에 배추가 있는데 방문하지 않은 경우
-                                visit[nx][ny] = true;
-                                que.push({nx,ny});
-                            }
-                        }
+                    for(int dir=0; dir<4; dir++) {
+                        int nx = cur.first + dx[dir];
+                        int ny = cur.second + dy[dir];
+                        if(nx<0 || nx >= N || ny<0 || ny >=M) continue;
+                        if(board[nx][ny] != 1 || visit[nx][ny]) continue;
+                        q.push({nx,ny});
+                        visit[nx][ny] = true;
                     }
-                }
+                }  
             }
         }
-        cout << cnt<< '\n';
-    
+        cout << cnt << "\n";
+        
     }
     return 0;
 }
