92befc496d523d8507593d6aebdbad47dacaeebd
Mutatio02 <144579556+Mutatio02@users.noreply.github.com>
2024-11-19 15:49:18 +0900

    [Silver I] Title: 나이트의 이동, Time: 44 ms, Memory: 2508 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.cc
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.cc
@@ -1,60 +1,59 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
 #include <queue>
-
+#include <string>
 using namespace std;
-
-int dx[8] = {1, 2, -1, -2, 1, 2, -1, -2};
-int dy[8] = {2, 1, -2, -1, -2, -1, 2, 1};
+// 24.11.19 -- 나이트 이동 복습
 int board[301][301];
+int dx[8] = {1 , 2, -1, -2, -1, -2, 1, 2};
+int dy[8] = {2 , 1, -2, -1, 2, 1, -2,-1};
 
-int main(void) {
+
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int T;
     cin >> T;
-    while (T--) {
-        int I;
-        cin >> I;
 
-        for (int i = 0; i < I; i++) {
-            fill(board[i], board[i] + I, -1);
+    while(T--) {
+        int l;
+        cin >> l;
+        queue<pair<int,int>> q;
+        for(int i=0; i<l; i++) {
+            for(int j=0; j<l; j++) {
+                board[i][j] = -1;
+            }
         }
-
-        queue<pair<int, int>> q;
-        pair<int, int> loc;
-        pair<int, int> mov;
+        
+        pair<int,int> loc;
+        pair<int,int> mov;
         cin >> loc.first >> loc.second;
         cin >> mov.first >> mov.second;
 
-        if (loc == mov) {
-            cout << 0 << "\n";
+        if(loc == mov) {
+            cout << 0 << '\n';
             continue;
         }
 
-        q.push({loc.first, loc.second});
+        q.push({loc.first,loc.second});
         board[loc.first][loc.second] = 0;
 
-        while (!q.empty()) {
+        while(!q.empty()) {
             auto cur = q.front();
             q.pop();
 
-            for (int i = 0; i < 8; i++) {  
-                int nx = cur.first + dx[i];
-                int ny = cur.second + dy[i];
+            for(int dir = 0; dir<8; dir++) {
+                int nx = cur.first + dx[dir];
+                int ny = cur.second + dy[dir];
 
-                if (nx < 0 || nx >= I || ny < 0 || ny >= I) continue;
-                if (board[nx][ny] >= 0) continue;
-
-                q.push({nx, ny});
-                board[nx][ny] = board[cur.first][cur.second] + 1;
+                if(nx<0 || nx >= l || ny<0 || ny >= l) continue;
+                if(board[nx][ny] >= 0) continue;
+                board[nx][ny] = board[cur.first][cur.second] +1;
+                q.push({nx,ny});
             }
         }
+        cout << board[mov.first][mov.second] << '\n';
 
-        cout << board[mov.first][mov.second] << "\n";
     }
-
     return 0;
 }
