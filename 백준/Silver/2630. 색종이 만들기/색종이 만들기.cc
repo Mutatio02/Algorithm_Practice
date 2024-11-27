c2f42b14c22f4112c3fd14144aa92be794958909
Mutatio02 <144579556+Mutatio02@users.noreply.github.com>
2024-11-27 17:07:42 +0900

    [Silver II] Title: 색종이 만들기, Time: 0 ms, Memory: 2084 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
@@ -1,48 +1,50 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
 
 int board[129][129];
-int white = 0, blue = 0;
-
-void Search(int x,int y, int size) {
-    int tmp = board[x][y];
-    bool isEqual = true;
-    for(int i=x; i<x+size; i++) {
-        for(int j=y; j<y+size; j++) {
-            if(board[i][j] != tmp) {
-                isEqual = false;
-                break;
-            }
+int N;
+int white = 0;
+int blue = 0;
+// 24.11.27 -- 색종이 만들기 복습
+bool IsEqual(int r,int c, int size) {
+    for(int i= r; i<r+size; i++) {
+        for(int j=c; j<c+size; j++) {
+            if(board[r][c]!=board[i][j])
+                return false;
         }
-        if(!isEqual)    break;
     }
-    if(isEqual) {
-        if(tmp ==0) white++;
-        else    blue++;
-    }
-    else {
-        int tmpsize = size/2;
-        Search(x,y,tmpsize); // I
-        Search(x+tmpsize,y,tmpsize); // II
-        Search(x,y+tmpsize,tmpsize); // III
-        Search(x+tmpsize,y+tmpsize,tmpsize); // IV
-    }
+    return true;
 }
-int main(void) {
+void partition(int r,int c,int idx) {
+    if(IsEqual(r,c,idx)) {
+        if(board[r][c] == 0) {
+            white++;
+        }
+        else{
+            blue++;
+        }
+        return;
+    }
+    int div = idx/2;
+
+    partition(r,c,div);
+    partition(r,c+div,div);
+    partition(r+div,c,div);
+    partition(r+div,c+div,div);
+    
+}
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int N;
     cin >> N;
     for(int i=0; i<N; i++) {
         for(int j=0; j<N; j++) {
             cin >> board[i][j];
-        } // 파랑은 1 하양은 0
-    }
-    Search(0,0,N);    
-    cout << white << '\n' << blue;
+        }
+    }    
+    partition(0,0,N);
+
+    cout << white << "\n" << blue;
     return 0;
 }
