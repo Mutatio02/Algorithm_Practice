b82c66580d972cdbffb9af10bb25281f127a8e0c
Mutatio02 <144579556+Mutatio02@users.noreply.github.com>
2024-12-24 15:41:12 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2200 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,46 +1,43 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <queue>
-// 24.11.09 복습 -- 미로찾기
+#include <bits/stdc++.h>
 using namespace std;
+// 24.12.24 복습 -- 미로
+int N,M;
+string maze[101];
+int vis[101][101];
 
-int dx[4] = {0,1,0,-1};
-int dy[4] = {1,0,-1,0};
-string board[101];
-int visit[101][101];
+int dx[4] ={0,1,0,-1};
+int dy[4] ={1,0,-1,0};
 
-int main(void) {
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int N,M;
     cin >> N >> M;
+    queue<pair<int,int>> que;
 
     for(int i=0; i<N; i++) {
-        cin >> board[i];
+        cin >> maze[i];
     }
 
-    queue<pair<int,int>> que;
     que.push({0,0});
-    visit[0][0] = 1;
+    vis[0][0] = 1;
 
     while(!que.empty()) {
         auto cur = que.front();
         que.pop();
 
         for(int i=0; i<4; i++) {
-            int nx = cur.first + dx[i];
-            int ny = cur.second + dy[i];
+            int nx = dx[i] + cur.first;
+            int ny = dy[i] + cur.second;
 
-            if(nx<0 || nx >= N || ny<0 || ny>=M)    continue; // 범위 지나면 무시
-            if(board[nx][ny] == '0'|| visit[nx][ny] != 0) continue;
-
+            if(nx<0 || nx>=N || ny<0 || ny>=M) continue;
+            if(maze[nx][ny] == '0' || vis[nx][ny] != 0) continue;
             que.push({nx,ny});
-            visit[nx][ny] = visit[cur.first][cur.second] + 1;
+            vis[nx][ny] = vis[cur.first][cur.second] + 1;
         }
     }
-    cout << visit[N-1][M-1];
+
+    cout << vis[N-1][M-1];
 
     return 0;
 }
