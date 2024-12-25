4185a26941dacdb03dd0c66a7cdd93bf123e7576
Mutatio02 <144579556+Mutatio02@users.noreply.github.com>
2024-12-25 17:42:38 +0900

    [Gold III] Title: 불!, Time: 40 ms, Memory: 10964 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.cc
+++ 백준/Gold/4179. 불！/불！.cc
@@ -1,41 +1,39 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <queue>
-// 24.11.10 -- 복습
+#include <bits/stdc++.h>
 using namespace std;
+// 24.12.25 복습 -- 불!
+int R,C;
 
-int dx[4]= {0,1,0,-1};
-int dy[4] = {1,0,-1,0};
+int dx[4] ={0,1,0,-1};
+int dy[4] ={1,0,-1,0};
 
-char board[1001][1001];
-int visit[1001][1001];
-int fire[1001][1001];
-
-int main(void) {
+char maze[1001][1001];
+int fire[1001][1001]; // 불
+int vis[1001][1001]; // 지훈
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int R,C;
     cin >> R >> C;
 
-    queue<pair<int,int>> q;
+    for (int i=0; i<R; i++) {
+        fill(fire[i], fire[i]+C, -1);
+        fill(vis[i], vis[i]+C, -1);
+    }
+    queue<pair<int,int>> que;
     queue<pair<int,int>> f;
+    int time = 0;
+    
     for(int i=0; i<R; i++) {
         for(int j=0; j<C; j++) {
-            cin >> board[i][j];
+            cin >> maze[i][j];
 
-            if(board[i][j] == 'J') { // 초기위치 방문
-                q.push({i,j});
-                visit[i][j] = 1; // 방문표시
+            if(maze[i][j] == 'J') {
+                que.push({i,j});
+                vis[i][j] = 1;
             }
-            else if(board[i][j] == 'F') { // 불난 위치 
+            if(maze[i][j] == 'F') {
                 f.push({i,j});
-                fire[i][j] = 1; // 불난 표시
-            }
-            else {
-                visit[i][j] = 0;
-                fire[i][j] = 0;
+                fire[i][j] = 1;
             }
         }
     }
@@ -43,38 +41,39 @@ int main(void) {
     while(!f.empty()) {
         auto cur = f.front();
         f.pop();
+
         for(int i=0; i<4; i++) {
-            int nx = cur.first + dx[i];
-            int ny = cur.second + dy[i];
+            int nx = dx[i] + cur.first;
+            int ny = dy[i] + cur.second;
 
-            if(nx<0 || nx>=R || ny<0 || ny >= C) continue;
-            if(board[nx][ny] == '#' || fire[nx][ny] != 0) continue; // 불의 위치가 벽이거나 이미 이미 방문한 것은 무시한다.
-
-            fire[nx][ny] = fire[cur.first][cur.second] +1;
+            if(nx<0 || nx>=R || ny<0 || ny>=C) continue;
+            if(maze[nx][ny] == '#' || fire[nx][ny]!=-1) continue;
+            fire[nx][ny] = fire[cur.first][cur.second]+1;
             f.push({nx,ny});
         }
     }
 
-    while(!q.empty()) {
-        auto cur = q.front();
-        q.pop();
-        for(int i=0; i<4; i++) {
-            int nx = cur.first + dx[i];
-            int ny = cur.second + dy[i];
+    while(!que.empty()) {
+        auto cur = que.front();
+        que.pop();
 
-            if(nx<0 || nx>=R || ny<0 || ny >= C) { // 범위를 지나간 것은 통과에 성공한 것이다.
-                cout << visit[cur.first][cur.second];
+        for(int i=0; i<4; i++) {
+            int nx = dx[i] + cur.first;
+            int ny = dy[i] + cur.second;
+
+            if(nx<0 || nx>=R || ny<0 || ny>=C) { // R과 C의 범위를 넘어서면 탈출
+                cout << vis[cur.first][cur.second];
                 return 0;
             }
-            if(board[nx][ny] == '#' || visit[nx][ny] != 0) continue; // 방문 위치가 벽이거나 이미 방문한 것은 무시한다.
-            if(fire[nx][ny] != 0 && fire[nx][ny] <= visit[cur.first][cur.second] +1) continue; 
-            // 이미 불이 번졌거나 도착 시점과 불의 위치 전파 위치가 같은 경우는 무시한다.
+            if(maze[nx][ny] == '#' || vis[nx][ny] !=-1) continue;
+            if(fire[nx][ny] !=-1 && fire[nx][ny] <= vis[cur.first][cur.second]+1) continue;
             
-            visit[nx][ny] = visit[cur.first][cur.second] +1;
-            q.push({nx,ny});
+            vis[nx][ny] = vis[cur.first][cur.second] +1;
+            que.push({nx,ny});
         }
     }
-
+    
     cout << "IMPOSSIBLE";
+
     return 0;
 }
