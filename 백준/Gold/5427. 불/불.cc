8ed4feea97ea85b081f5549cc73abb6f7f5f1c55
Mutatio02 <144579556+Mutatio02@users.noreply.github.com>
2024-12-29 16:33:27 +0900

    [Gold IV] Title: 불, Time: 56 ms, Memory: 12968 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.cc
+++ 백준/Gold/5427. 불/불.cc
@@ -1,40 +1,40 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <queue>
-
+#include <bits/stdc++.h>
 using namespace std;
+// 24.12.29 --불 복습
+char board[1001][1001];
+int vis[1001][1001];
+int fire[1001][1001];
+
+int T,w,h;
 
 int dx[4] = {0,1,0,-1};
 int dy[4] = {1,0,-1,0};
-char board[1001][1001];
-int visit[1001][1001];
-int fire[1001][1001];
-int main(void) {
+
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int T;
     cin >> T;
     while(T--) {
-        int w,h;
-        cin >> w >> h; // 너비와 높이
-        queue<pair<int,int>> q; // 상근
-        queue<pair<int,int>> f; // 불
+        cin >> w >> h;
+        queue<pair<int,int>> que;
+        queue<pair<int,int>> f;
         bool escape = false;
+
         for(int i=0; i<h; i++) {
             for(int j=0; j<w; j++) {
                 cin >> board[i][j];
-                visit[i][j] = -1;
-                fire[i][j] = -1; 
+                vis[i][j] = -1;
+                fire[i][j] = -1; // 초기화
 
-                if(board[i][j] == '*') { // 불
-                    f.push({i,j});
-                    fire[i][j] = 0;
+                if(board[i][j] == '@') {
+                    que.push({i,j});
+                    vis[i][j] = 0; // 방문 시작
                 }
-                else if(board[i][j] == '@') { // 시작 위치
-                    q.push({i,j});
-                    visit[i][j] = 0;
+
+                if(board[i][j] == '*') {
+                    f.push({i,j});
+                    fire[i][j] = 0; // 불 시작
                 }
             }
         }
@@ -43,42 +43,40 @@ int main(void) {
             auto cur = f.front();
             f.pop();
 
-            for(int dir = 0; dir<4; dir++) {
-                int nx = dx[dir] + cur.first;
-                int ny = dy[dir] + cur.second;
+            for(int i=0; i<4; i++) {
+                int nx = dx[i] + cur.first;
+                int ny = dy[i] + cur.second;
 
-                if(nx<0 || nx >= h || ny<0 || ny >= w) continue;
-                if(board[nx][ny] =='#' || fire[nx][ny] != -1) continue;
-                fire[nx][ny] = fire[cur.first][cur.second] +1;
+                if(nx<0 || nx >= h || ny<0 || ny>=w) continue;
+                if(board[nx][ny] == '#' || fire[nx][ny] != -1) continue;
+                fire[nx][ny] = fire[cur.first][cur.second]+1;
                 f.push({nx,ny});
             }
         }
-        
-        while(!q.empty() && !escape) { // 탈출할 때 까지
-            auto cur = q.front();
-            q.pop();
-            
-            for(int dir = 0; dir<4; dir++) {
-                int nx = dx[dir] + cur.first;
-                int ny = dy[dir] + cur.second;
 
-                if(nx<0 || nx >= h || ny<0 || ny>= w)  {
-                    cout << visit[cur.first][cur.second] + 1 << "\n";
+        while(!que.empty() && !escape) {
+            auto cur = que.front();
+            que.pop();
+            
+            for(int i=0; i<4; i++) {
+                int nx = dx[i] +cur.first;
+                int ny = dy[i] +cur.second;
+
+                if(nx<0 || nx >= h || ny<0 || ny >= w) {
+                    cout << vis[cur.first][cur.second]+1 << "\n";
                     escape = true;
                     break;
                 }
-                if(visit[nx][ny] != -1 || board[nx][ny] == '#') continue; // 이미 방문한 경우
-                if(fire[nx][ny] != -1 && fire[nx][ny]<=visit[cur.first][cur.second]+1) continue; // 이미 빨리 도착한 경우 
-                
-                visit[nx][ny] = visit[cur.first][cur.second] +1;
-                q.push({nx,ny});
+
+                if(board[nx][ny] == '#' || vis[nx][ny] != -1 ) continue;
+                if(fire[nx][ny] != -1 && vis[cur.first][cur.second]+1 >= fire[nx][ny]) continue;
+                vis[nx][ny] = vis[cur.first][cur.second]+1;
+                que.push({nx,ny});
             }
         }
+
         if(!escape) {
             cout << "IMPOSSIBLE" << "\n";
         }
-
     }
-    
-    return 0;
 }
