d227ec6e687954620a4a30a65d0528dffaebb97b
Mutatio02 <144579556+Mutatio02@users.noreply.github.com>
2024-12-30 16:31:55 +0900

    [Silver I] Title: 안전 영역, Time: 20 ms, Memory: 2072 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.cc
+++ 백준/Silver/2468. 안전 영역/안전 영역.cc
@@ -1,17 +1,33 @@
-#include <iostream>
-#include <algorithm>
-#include <queue>
-
+#include <bits/stdc++.h>
 using namespace std;
+// 24.12.30 -- 안전영역 복습
+
+int N;
 int board[101][101];
-int visit[101][101];
+bool vis[101][101];
+
 int dx[4] = {0, 1, 0, -1};
 int dy[4] = {1, 0, -1, 0};
 
-// 방문 배열 초기화 함수
-void clear(int size) {
-    for (int i = 0; i < size; i++) {
-        fill(visit[i], visit[i] + size, -1);
+void bfs(int x, int y, int limit) {
+    queue<pair<int, int>> que;
+    que.push({x, y});
+    vis[x][y] = true; 
+
+    while (!que.empty()) {
+        auto cur = que.front();
+        que.pop();
+
+        for (int i = 0; i < 4; i++) {
+            int nx = dx[i] + cur.first;
+            int ny = dy[i] + cur.second;
+
+            if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+            if (board[nx][ny] <= limit || vis[nx][ny]) continue; // limit(height) 보다 작은 경우 무시
+
+            vis[nx][ny] = true;  
+            que.push({nx, ny});
+        }
     }
 }
 
@@ -19,54 +35,38 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int N;
     cin >> N;
 
-    int H = 0; // 최대 높이
+    int height = 0;  // 최대 높이
+    int cnt = 0;     // 최대 안전영역 개수
+
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             cin >> board[i][j];
-            H = max(H, board[i][j]); // 최대 높이 계산
+            height = max(height, board[i][j]);
         }
     }
 
-    int area = 0; // 최대 안전 영역 수
-    for (int h = 0; h <= H; h++) { // 높이 0부터 최대 높이까지
-        clear(N); 
-        int cnt = 0; 
+    for (int h = 0; h <= height; h++) {
+        int area = 0; 
+
+        for (int i = 0; i < N; i++) {
+            fill(vis[i], vis[i] + N, false);  // 초기화 --> 높이에 따라 달라지기 때문
+        }
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                // 안전 영역 시작 조건
-                if (board[i][j] > h && visit[i][j] == -1) {
-                    cnt++; // 새로운 안전 영역 발견
-                    queue<pair<int, int>> q;
-                    q.push({i, j});
-                    visit[i][j] = 1;
-
-                    while (!q.empty()) {
-                        auto cur = q.front();
-                        q.pop();
-
-                        for (int dir = 0; dir < 4; dir++) {
-                            int nx = cur.first + dx[dir];
-                            int ny = cur.second + dy[dir];
-
-                            if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
-                            if (board[nx][ny] <= h || visit[nx][ny] != -1) continue;
-     
-                            q.push({nx, ny});
-                            visit[nx][ny] = 1;
-                        }
-                    }
+                if (board[i][j] > h && !vis[i][j]) {  // 방문하지 않고 안전영역(높이보다 큰 경우)
+                    bfs(i, j, h);
+                    area++;  // 하나의 영역이 확정됨
                 }
             }
         }
 
-        area = max(area, cnt); 
+        cnt = max(area, cnt);
     }
 
-    cout << area << '\n';
+    cout << cnt;  
 
     return 0;
 }
