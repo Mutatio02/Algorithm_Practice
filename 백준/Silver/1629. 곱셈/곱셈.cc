706168e7cd3f54dc3ce94c61859f5b06c19a3b8e
Mutatio02 <144579556+Mutatio02@users.noreply.github.com>
2025-01-01 16:49:07 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -1,24 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long calculate(long long a,long long b, long long c) {
-    if(b == 1) return a%c; // b == 1 이면 결과 값은 a%c
-
-    long long tmp = calculate(a,b/2,c); // 지수 법칙
-    tmp = tmp*tmp % c;
-
+long long A,B,C;
+long long solve(long long a,long long b, long long c) {
+    if(b==1) return a%c; // b가 1이면 결과는 a%c;
+    long long tmp = solve(a,b/2,c);
+    tmp= tmp*tmp%c;
     if(b%2 ==0) return tmp;
-    return tmp*a % c; 
+    return tmp*a%c;
 }
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    long long A,B,C;
     cin >> A >> B >> C;
-    
-    cout << calculate(A,B,C);
+    long long result = solve(A,B,C);
 
+    cout << result;
     return 0;
 }
