ec5f51ca61781e32058e2714a1f9562ea0261c34
Mutatio02 <144579556+Mutatio02@users.noreply.github.com>
2025-01-01 20:30:07 +0900

    [Gold V] Title: Z, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.cc
+++ 백준/Gold/1074. Z/Z.cc
@@ -1,30 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
+// 25.01.01  Z 복습
+int N,r,c;
 
-
-int visit(int n,int r, int c) {
-    if(n == 0) return 0;
-    int length = 1<<(n-1); // 길이
-
-    if(r<length && c <length)   
-        return visit(n-1,r,c); // 왼쪽 위
-
-    if(r<length && c>= length)
-        return length*length + visit(n-1,r,c-length); // 오른쪽 위
-
-    if(r>=length && c<length)
-        return length*length*2 + visit(n-1,r-length,c); // 왼쪽 아래
-
-    
-    return length*length*3 + visit(n-1,r-length,c-length); // 오른쪽 아래
+int solve (int n,int r,int c) {
+    if(n==0) return 0;
+    int len = 1 << (n-1);
+    // 왼쪽 위
+    if(r<len && c<len) return solve(n-1,r,c);
+    // 오른쪽 위
+    if(r<len && c>=len) return len*len + solve(n-1,r,c-len);
+    // 왼쪽 아래
+    if(r>=len && c<len) return len*len*2 + solve(n-1,r-len,c);
+    // 오른쪽 아래
+    return len*len*3 + solve(n-1,r-len,c-len);
 }
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int N,r,c;
-    cin >> N >> r >> c;
-    cout << visit(N,r,c);
 
+    cin >> N >> r >> c;
+    int result = solve(N,r,c);
+
+    cout << result;
     return 0;
 }
