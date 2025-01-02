8ecaa1c304b51d056f64f4b71b5f2b4d29ab09b5
Mutatio02 <144579556+Mutatio02@users.noreply.github.com>
2025-01-02 16:42:54 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.cc
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.cc
@@ -1,33 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
+// 25.01.02  복습
+int N;
 
-void output(int n, int tmp) {
-    string str = string(tmp*4, '_');
-
-    cout << str << "\"재귀함수가 뭔가요?\"\n";
-
-    if (n == 0) { // base condition
+void solve(int n) {
+    string str = string(n*4,'_');
+    
+    cout <<str <<"\"재귀함수가 뭔가요?\"\n";
+    if(n==N) {
         cout << str << "\"재귀함수는 자기 자신을 호출하는 함수라네\"\n";
         cout << str << "라고 답변하였지.\n";
         return;
-    }
-
-    cout << str << "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n";
-    cout << str << "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n";
-    cout << str << "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n";
-
-    output(n - 1,tmp + 1); 
-
-    cout << str << "라고 답변하였지.\n"; 
+    } 
+    
+    cout <<str <<"\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n";
+    cout <<str <<"마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n";
+    cout <<str <<"그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n";
+    solve(n+1);
+    cout <<str << "라고 답변하였지.\n";
 }
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
-    int N;
+
     cin >> N;
+
     cout << "어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n";
-    output(N, 0);
+    solve(0);
+    
     return 0;
 }
