9bf3efe1d3a7f43c0440d236a77ba71ab3f88c8d
Mutatio02 <144579556+Mutatio02@users.noreply.github.com>
2025-01-02 22:51:43 +0900

    [Silver II] Title: 종이의 개수, Time: 332 ms, Memory: 30520 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.cc
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.cc
@@ -1,44 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+// 25.01.02  재귀복습
 int N;
-int board[3001][3001];
-int cnt[3];
-bool check(int s,int e, int size) { // 범위에 맞추어 같은지 아닌지 비교
-    for(int i = s; i<s+size; i++) {
-        for(int j=e; j<e+size; j++) {
-            if(board[s][e] != board[i][j])
+int paper[2701][2701];
+int cnt[3]; // -1(0) 0(1) 1(2)의 개수를 담을 배열
+bool check(int a,int b, int size) {
+    for(int i=a; i<a+size; i++) {
+        for(int j=b; j<b+size; j++) {
+            if(paper[a][b]!=paper[i][j])
                 return false;
         }
     }
     return true;
 }
-void partition(int start,int end, int size) {
-    if(check(start,end,size)) {
-        cnt[board[start][end]+1] +=1; // -1 0 1 값이 각각 0 1 2 idx에 해당됨
+void solve(int x,int y, int size) {
+    if(check(x,y,size)) {
+        cnt[paper[x][y]+1] +=1;
         return;
     }
-    int newsize = size / 3;
-
+    int newsize = size/3;
     for(int i=0; i<3; i++) {
         for(int j=0; j<3; j++) {
-            partition(start + i*newsize,end + j*newsize,newsize);
+            solve(x+i * newsize, y+j * newsize, newsize);
         }
     }
-    
 }
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
-    int N;
+
     cin >> N;
     for(int i=0; i<N; i++) {
-        for(int j= 0; j<N; j++) {
-            cin >> board[i][j];
+        for(int j=0; j<N; j++) {
+            cin >> paper[i][j];
         }
-    }    
-    partition(0,0,N);
+    }
+
+    solve(0,0,N);
 
     for(int i=0; i<3; i++) {
         cout << cnt[i] << "\n";
