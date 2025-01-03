694c47aed583a1d267b102b98e1878b896234d8e
Mutatio02 <144579556+Mutatio02@users.noreply.github.com>
2025-01-03 14:02:40 +0900

    [Silver III] Title: N과 M (1), Time: 16 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,34 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int board[10]; // 배열에 수들을 저장해 놓기
-bool IsUsed[10];// 이전으로 돌아가야 되는지 체크
-int N,M;
-
+// 25.01.03 백트레킹 복습 (N과 M)
+int N,M; // N 까지의 수 M개 선택
+bool used[10]; 
+int board[10];
 void back_track(int k) {
-    if(k == M) { // 길이가 m인 수열 완성
+    if(k == M) { // 길이가 M이 되면 출력
         for(int i=0; i<M; i++) {
             cout << board[i] << ' ';
         }
-        cout << "\n";
+        cout<< "\n";
         return;
     }
-
     for(int i=1; i<=N; i++) {
-        if(!IsUsed[i]) { // IsUsed[i]가 사용되지 않았다면
-            board[k] = i; // 저장
-            IsUsed[i] = true; // 사용되었 다는 표시 남기기
-            back_track(k+1); // 다음 길이 탐색
-            IsUsed[i] = false; // 백트래킹으로 되돌리기
+        if(!used[i]) { // 사용되지 않았으면
+            board[k] = i; // 길이가 K일때
+            used[i] = true; // i는 사용됨
+            back_track(k+1); //  다음 길이 탐색 및 백트래킹
+            used[i] = false; // 백트래킹으로 원상태 복귀
         }
     }
 }
+
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    cin >> N >> M; // 수의 범위, 길이
-
+    cin >> N >> M; 
     back_track(0);
 
     return 0;
