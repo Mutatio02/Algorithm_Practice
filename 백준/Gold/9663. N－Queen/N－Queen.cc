000839e1247cc0d8a54a9d6ad7c121ff8863531d
Mutatio02 <144579556+Mutatio02@users.noreply.github.com>
2025-01-04 15:32:50 +0900

    [Gold IV] Title: N-Queen, Time: 1632 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -1,45 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
+// 25.01.04 백트레킹 복습 (N-Queen)
 
 int N;
+bool line1[31]; // 열 
+bool line2[31]; // 왼쪽 아래 대각선
+bool line3[31]; // 오른쪽 아래 대각선
 int cnt = 0;
-bool first[32]; // 같은 열
-bool second[32]; // 왼쪽 대각선 
-bool third[32]; // 오른쪽 대각선
-/* N의 최대는 15(16이라 가정) 대각선의 개수를 나타내기 위해선 
-   2*N -1 성립 필요 */
-
-void solve(int cur) { // cur가 N번쨰 행 도착 -- 즉 (cur가 행) 
-    if(cur == N) { // N번쨰 행까지 넣는데 성공한 경우
-        cnt++; // 수를 증가
+void back_track(int cur) { // 행
+    if(cur==N) {
+        cnt++;
         return;
     }
 
     for(int i=0; i<N; i++) { // 열
-        if(first[i] || second[cur+i] || third[cur-i+N-1]) {
-            continue; // 이미 해당 열이나 대각선에 도착한 경우 무시
+        if(line1[i] || line2[i+cur] || line3[cur-i+N-1]) {
+            continue;
         }
+        
+        line1[i] = true;
+        line2[i+cur] = true;
+        line3[cur-i+N-1] = true;
 
-        first[i] = true;  
-        second[cur+i] = true; 
-        third[cur-i+N-1] = true; 
-        solve(cur+1);
+        back_track(cur+1);
 
-        first[i] = false;
-        second[cur+i] = false;
-        third[cur-i+N-1] = false;
+        line1[i] = false;
+        line2[i+cur] = false;
+        line3[cur-i+N-1] = false;
     }
 }
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    cin >> N;
-
-    solve(0);
+    cin >> N; 
+    back_track(0);
 
     cout << cnt;
-
     return 0;
 }
