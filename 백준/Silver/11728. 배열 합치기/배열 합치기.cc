83731d724fe4ffcf25144584d4335ddd5523d94f
Mutatio02 <144579556+Mutatio02@users.noreply.github.com>
2025-01-31 19:36:43 +0900

    [Silver V] Title: 배열 합치기, Time: 428 ms, Memory: 33280 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.cc
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.cc
@@ -1,34 +1,45 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
+// 25.1.31 배열 합치기(정렬 I) 
+long long N,M;
 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-int main(void) {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int N,M;
     cin >> N >> M;
+    vector<long long> a(N,0);
+    vector<long long> b(M,0);
+    vector<long long> c(N+M,0);
 
-    vector<int> A(N,0);
-    vector<int> B(M,0);
-    vector<int> C;
     for(int i=0; i<N; i++) {
-        cin >> A[i];
-        C.push_back(A[i]);
+        cin >> a[i];
     }
-    
-    for(int j=0; j<M; j++) {
-        cin >> B[j];
-        C.push_back(B[j]);
-    }
-    
-    sort(C.begin(),C.end());
 
-    for(int i=0; i<C.size(); i++) {
-        cout << C[i] << " ";
+    for(int j=0; j<M; j++) {
+        cin >> b[j];
     }
     
+    int aidx = 0;
+    int bidx = 0;
+
+    for(int i=0; i<N+M; i++) {
+        if(aidx == N) { // 배열 a에 있는 원소를 모두 채우면
+            c[i] = b[bidx++]; // b에 있는 원소 채우기
+        }
+        else if(bidx == M) { // 배열 b에 있는 원소를 모두 채우면
+            c[i] = a[aidx++]; // a에 있는 원소 채우기
+        }
+        else if(a[aidx]<=b[bidx]) { 
+            c[i] = a[aidx++];
+        }
+        else {
+            c[i] = b[bidx++];
+        }
+    }
+
+    for(int i=0; i<N+M; i++) {
+        cout << c[i] <<' ';
+    }
     return 0;
 }
