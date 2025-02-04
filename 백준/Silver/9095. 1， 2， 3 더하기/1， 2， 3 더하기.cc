f8c4f165f437ebf4a2a9dd18498f257059f070ad
Mutatio02 <144579556+Mutatio02@users.noreply.github.com>
2025-02-04 15:59:33 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -1,31 +1,27 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
+// 25.2.4 1,2,3 더하기(DP 복습) 
+int N;
+int dp[15];
 
-int main(void) {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
-    int dp[12] = {0};
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-    int T;
-    cin >> T;
+    cin >> N;
+    // 1,2,3을 이용해 만들 수 있는 경우
+    dp[1] = 1; // (1)
+    dp[2] = 2; // (1,1) , (2)
+    dp[3] = 4; // (1,1,1) , (1,2) , (2,1) , (3)
 
-    dp[1] = 1; // 1
-    dp[2] = 2; // (1,1)과 2
-    dp[3] = 4; // (1,1,1) (1,2) (2,1) 과 3
-
-    for(int i=4; i<12; i++) {
-        dp[i] = dp[i-1] + dp[i-2] + dp[i-3]; // 다음 점화식은 4부터 성립
-    }
-
-    while(T--) {
-        int n;
-        cin >> n;
-        cout << dp[n] << '\n';
+    for(int i=4; i<15; i++) {
+        dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
     }
     
+    while(N--) {
+        int num;
+        cin >> num;
+        cout << dp[num] << "\n";
+    }
     return 0;
 }
