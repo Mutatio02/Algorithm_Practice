20400f8aad9a25db6a07b211f557ca3e6eee3142
Mutatio02 <144579556+Mutatio02@users.noreply.github.com>
2025-02-06 02:02:47 +0900

    [Silver I] Title: 1로 만들기 2, Time: 8 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
@@ -1,40 +1,37 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
+// 25.02.06 1로 만들기 2(dp복습)
+int N;
+int dp[1000001];
+int arr[1000001];
 
-int dp[1000001]; // 계산 횟수 dp
-int arr[1000001]; // 계산 하기 이전의 수 저장
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-int main(void) {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int N;
     cin >> N;
-    
+
+    dp[0] = 0;
     dp[1] = 0;
 
-    for(int i=2 ; i<=N; i++) {
-        dp[i] = dp[i-1] + 1; // 1로 뺴기
+    for(int i=2; i<=N; i++) {
+        dp[i] = dp[i-1]+1;
         arr[i] = i-1;
 
-        if(i%2 ==0 && dp[i] > dp[i/2] +1 ) {
-            dp[i] = dp[i/2] + 1; // 2로 나누기
+        if(i%2==0 && (dp[i] > dp[i/2]+1)) {
+            dp[i] = dp[i/2]+1;
             arr[i] = i/2;
         }
-        
-        if(i%3 == 0  && dp[i] > dp[i/3] + 1) {
-            dp[i] = dp[i/3] +1; // 3로 나누기
+        if(i%3==0 && (dp[i] > dp[i/3]+1)) {
+            dp[i] = dp[i/3]+1;
             arr[i] = i/3;
         }
     }
-    
-    cout << dp[N] << '\n';
-    int cur = N;
-    while(cur >0) {
-        cout << cur << ' ';
-        cur = arr[cur];
+    cout << dp[N] << "\n";
+    int search = N;
+
+    while(search>0) {
+        cout << search << ' ';
+        search = arr[search];
     }
-    return 0;
 }
