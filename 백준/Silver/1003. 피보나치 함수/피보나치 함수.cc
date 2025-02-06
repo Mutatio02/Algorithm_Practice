477b65b515f9e8494d35c9806a079d17f8389b6c
Mutatio02 <144579556+Mutatio02@users.noreply.github.com>
2025-02-06 14:56:16 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -1,29 +1,26 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <map>
+#include <bits/stdc++.h>
 using namespace std;
+// 25.02.06 피보나치 함수(dp복습)
+int T;
+int zero[45] = {1,0}; // 0의 개수 테이블
+int one[45] = {0,1}; // 1의 개수 테이블
 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-int main(void) {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
-    int T;
     cin >> T;
-    int zero[41] = {1,0}; // 1,0 이후 피보나치 수열
-    int one[41] = {0,1}; // 0,1 이후 피노나치 수열
 
+    
     while(T--) {
         int N;
         cin >> N;
-        
+
         for(int i=2; i<=N; i++) {
-            zero[i] = zero[i-1] +zero[i-2];
-            one[i] = one[i-1] + one[i-2];
+            zero[i] = zero[i-1]+zero[i-2];
+            one[i] = one[i-1]+one[i-2];
         }
-        cout << zero[N] << " " << one[N] << '\n';
+        cout << zero[N] << ' ' << one[N] << "\n";
     }
     
-    return 0;
 }
