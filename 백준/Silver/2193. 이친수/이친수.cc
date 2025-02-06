d02ca3e6851fa0a0762d93aef2e75a8fe983bf98
Mutatio02 <144579556+Mutatio02@users.noreply.github.com>
2025-02-07 01:28:47 +0900

    [Silver III] Title: 이친수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.cc
+++ 백준/Silver/2193. 이친수/이친수.cc
@@ -1,26 +1,23 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
+// 25.02.07 이친수(dp복습)
+int n;
+long long dp[91][2]; // n자리 자연수 끝이 0 또는 1
 
-long long dp[91][2]; // dp[n][0] 은 끝 자리가 0, dp[n][1]은 끝 자리가 1
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-int main(void) {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int N;
-    cin >> N;
+    cin >> n;
 
     dp[1][0] = 0;
     dp[1][1] = 1;
 
-    for(int i=2; i<=N; i++) {
-        dp[i][0] = dp[i-1][0] + dp[i-1][1];
+    for(int i=2; i<=n; i++) {
+        dp[i][0] = dp[i-1][0] +dp[i-1][1];
         dp[i][1] = dp[i-1][0];
-     }
+    }
 
-    cout << dp[N][0] + dp[N][1];
-
-    return 0;
+    cout << dp[n][0] + dp[n][1];
+    
 }
