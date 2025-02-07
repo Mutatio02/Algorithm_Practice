362f3f8a9fd4e1bc50489a297ee46bb26d9acbe9
Mutatio02 <144579556+Mutatio02@users.noreply.github.com>
2025-02-08 00:21:50 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -1,32 +1,32 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
+// 25.02.08 가장 긴 증가하는 부분 수열(dp복습)
+int N;
+int arr[1010];
+int dp[1010];
 
-int dp[1001];
-int arr[1001];
 
-int main(void) {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-    int N;
     cin >> N;
-
-    for(int i=0; i<N; i++) {
+    
+    for(int i=1; i<=N; i++) {
         cin >> arr[i];
-        dp[i] = 1; // 길이를 1로 초기화
+        dp[i] = 1; // 1로 초기화
     }
     
-    for(int i=0; i<N; i++) {
-        for(int j=0; j<i; j++) {
-            if(arr[i] > arr[j]) {
-                dp[i] = max(dp[i],dp[j]+1); // 길이를 1로 늘리기
+    for(int i=1; i<=N; i++) {
+        for(int j=1; j<i; j++) {
+            if(arr[i] >arr[j]) {
+                dp[i] = max(dp[i],dp[j]+1); // rl
             }
         }
     }
-    int result = *max_element(dp,dp+N);
-    cout << result;
-     
+
+    int ans = *max_element(dp+1,dp+N+1);
+    cout << ans;
+
     return 0;
 }
