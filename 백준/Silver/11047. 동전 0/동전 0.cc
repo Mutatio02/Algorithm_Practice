8221678030430a7587b0438b403d3442cb834b0e
Mutatio02 <144579556+Mutatio02@users.noreply.github.com>
2025-02-09 18:27:46 +0900

    [Silver IV] Title: 동전 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.cc
+++ 백준/Silver/11047. 동전 0/동전 0.cc
@@ -1,38 +1,27 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <set>
+#include <bits/stdc++.h>
 using namespace std;
+// 25.02.09 동전 0 (그리디)
 
-int main(void) {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
-    int N,K;
+int N;
+long long K;
+int coin[15];
+int cnt = 0;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
     cin >> N >> K;
 
-    vector<int> v(N);
-    int poz = N-1; // 맨 마지막으로 초기화
-    for(int i=0; i<N; i++) {
-        cin >> v[i];
-
-        if(v[i] > K) {
-            poz = i-1;
-        }           
+    for(int i=1; i<=N; i++) {
+        cin >> coin[i];
     }
 
-    sort(v.begin(),v.end());
-
-    
-    int cnt = 0;
-    for(int i=poz; i>=0; i--) {
-        cnt += K/v[i];
-        K = K%v[i];
+    for(int i=N; i>=1; i--) { // 최소 개수를 위해 큰 값부터 
+        cnt += K/coin[i];
+        K%=coin[i];
     }
     cout << cnt;
-
-    
-    
 
     return 0;
 }
