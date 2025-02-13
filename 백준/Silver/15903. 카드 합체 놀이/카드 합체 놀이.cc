ee6bedb469cb491839b5d3a754434bb4cf7a6118
Mutatio02 <144579556+Mutatio02@users.noreply.github.com>
2025-02-13 16:43:59 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.cc
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.cc
@@ -8,24 +8,28 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    cin >> n >> m;
-    vector<long long> v(n);
-    for(int i=0; i<n; i++) {
-        cin >> v[i];
+    cin >> n >> m; // 최소 힙 사용(데이터 타입, 사용자료)
+    priority_queue<long long, vector<long long>, greater<long long>> pq;
+
+    while(n--) {
+        int num;
+        cin >> num;
+        pq.push(num);
     }
 
-    
     while(m--) {
-        swap(v[0],*min_element(v.begin(),v.end()));
-        swap(v[1],*min_element(v.begin()+1,v.end()));
-        long long sum = v[0] + v[1];
-        v[0] = sum;
-        v[1] = sum;
+        long long a = pq.top();
+        pq.pop();
+        long long b = pq.top();
+        pq.pop();
+        pq.push(a+b);
+        pq.push(a+b);
     }
 
     long long ans = 0;
-    for(int i=0; i<n; i++) {
-        ans += v[i];
+    while(!pq.empty()) {
+        ans += pq.top();
+        pq.pop();
     }
 
     cout << ans;
