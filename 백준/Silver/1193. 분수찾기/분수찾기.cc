d5f78bc18d23e5caa234042a7ff273d66d7d85dc
Mutatio02 <144579556+Mutatio02@users.noreply.github.com>
2025-02-16 23:38:37 +0900

    [Silver V] Title: 분수찾기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.cc
+++ 백준/Silver/1193. 분수찾기/분수찾기.cc
@@ -1,34 +1,27 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <string>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(void) {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+// 25.02.16 분수찾기 복습(수학) 
+long long X;
 
-    int X;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
     cin >> X;
 
     int line = 1;
-
     while(X>line) {
-        X -= line;
+        X-=line;
         line++;
     }
 
-    if(line%2 == 0) {
-        cout << X << "/" << line-X+1;
-    }
-    else {
-        cout << line-X+1 << "/" << X;
-    }
-    // 홀수 대각선 상승 -- 분자 감소, 분모 증가
+    if(line%2==1) cout << line+1-X << "/" << X; 
+    else cout << X << "/" << line+1-X;
 
-    // 짝수 대각선 하강 -- 분자 증가, 분모 감소
-    
+    // 1부터 N까지의 합 구하기
+    // 짝수 줄은 분모 감소 분자 증가
+    // 홀수 줄은 붐모 증가 분자 감소
     return 0;
-
 }
