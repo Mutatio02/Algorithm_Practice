6ece78de64a6881bcd19e8ff264d34653b9b83c0
Mutatio02 <144579556+Mutatio02@users.noreply.github.com>
2025-02-17 16:43:05 +0900

    [Silver II] Title: 베르트랑 공준, Time: 36 ms, Memory: 2164 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.cc
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.cc
@@ -1,44 +1,43 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cmath>
+#include <bits/stdc++.h>
 using namespace std;
-bool isPrime(int n) {
-    if(n==1)    return false;
-    if(n==2)    return true;
-    for(int i=2; i<=sqrt(n); i++) {
-        if(n%i == 0) {
-            return false;
+
+// 25.02.17 베르트랑 공준 복습(수학) 
+int N;
+
+void check(int x) {
+    int cnt = 0;
+    if(x==1) {
+        cout << 1 << "\n";
+        return;
+    }
+    vector<bool> prime(2*x+1,true); // 소수 판별 배열
+    prime[0] = prime[1] = false; //  0과 1은 소수가 아니다
+    for(int i=2; i*i<=2*x; i++) {
+        if(prime[i]) {
+            for(int j=i*i; j<=2*x; j+=i) { // 배수는 소수가 아니다
+                prime[j] = false;
+            }
         }
     }
-    return true;
+
+    for(int i=x+1; i<=2*x; i++) {
+        if(prime[i]) cnt++;
+    }
+
+    cout << cnt << "\n";
+    return;
+    
 }
 
-int main(void) {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-    vector<int> result;
-
-    while(true) {
-        int N;
+    while(1) {
         cin >> N;
-        if(N==0) {
-            break;
-        }
-
-        int count = 0;
-        for(int i=N+1; i<=2*N; i++) {
-            if(isPrime(i)) {
-                count++;
-            }
-        }  
-        result.push_back(count);
+        if(N==0) break;
+        check(N);
     }
     
-    for(int c : result) {
-        cout << c << '\n';
-    }
-
     return 0;
 }
