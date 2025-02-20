7cc82f9827e004bd7812809535c37f2b7109260d
Mutatio02 <144579556+Mutatio02@users.noreply.github.com>
2025-02-20 20:06:53 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -1,45 +1,45 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <cmath>
-#include <iomanip>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(void) {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+// 25.02.20 랜선 자르기 복습 (이분탐색) 
+int K,N;
+int line[10001];
 
-    int K,N;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
     cin >> K >> N;
-    vector<int> v(K);
-    int max_length = 0;
-    for(int i=0; i<K; i++){
-        cin >> v[i];
-        max_length = max(max_length,v[i]);
+    int length = 0;
+    for(int i=0; i<K; i++) {
+        cin >> line[i];
+        length = max(length,line[i]);
     }
 
-    sort(v.begin(),v.end());
-
-    long long left = 1;
-    long long right = max_length;
+    long long start = 1;
+    long long end = length;
     long long result = 0;
 
-    while(left<=right) {
-        long long middle = (left + right) / 2;
+    while(start<=end) {
+        long long mid = (start+end)/2;
         int cnt = 0;
+        
         for(int i=0; i<K; i++) {
-            cnt += v[i] / middle;
+            cnt += line[i]/mid; // mid로 나눴을떄
         }
 
-        if(cnt >=N) {
-            result = middle;
-            left = middle + 1;
+        if(cnt>=N) { // 개수가 N개 이상
+            result = mid; // 값은 mid가 될 것이다
+            start = mid+1;
         }
-        else {
-            right = middle -1;
-        }
+        else { // N개 미만
+            end = mid-1;
+        }   
+        
     }
 
     cout << result;
+
     return 0;
 }
