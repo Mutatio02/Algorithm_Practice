b840468abe112d60f4f6fb9a8562946dae30965a
Mutatio02 <144579556+Mutatio02@users.noreply.github.com>
2025-02-20 16:16:41 +0900

    [Silver IV] Title: 수 찾기, Time: 48 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,33 +1,42 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <map>
-
+#include <bits/stdc++.h>
 using namespace std;
-int main(void) {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
 
-    int N;
-    cin >> N;
-    
-    map<int,bool>mp;
-    int number;
-    for(int i=0; i<N; i++) {
-        cin >> number;
-        mp[number] = true;
+// 25.02.20 수 찾기 복습(이분탐색) 
+int N,M;
+int arr[100001];
+
+int binarysearch(int object) { // 이분탐색 직접 구현
+    int start = 0;
+    int end = N-1;
+    while(start<=end) {
+        int mid = (start+end)/2;
+        if(arr[mid] < object) {
+            start = mid+1;
+        }
+        else if(arr[mid] > object) {
+            end = mid-1;
+        }
+        else
+            return 1;
     }
-    int M;
-    cin >> M;
+    return 0;
+}
 
-    for(int i=0; i<M; i++) {
-        cin >>number;
-        if(mp[number]) {
-            cout << "1\n";
-        }
-        else {
-            cout << "0\n";
-        }
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N;
+    for(int i=0; i<N; i++) {
+        cin >> arr[i];
+    }
+
+    sort(arr,arr+N);
+    cin >> M;
+    while(M--) {
+        int num;
+        cin >> num;
+        cout << binarysearch(num) << "\n";
     }
     return 0;
 }
