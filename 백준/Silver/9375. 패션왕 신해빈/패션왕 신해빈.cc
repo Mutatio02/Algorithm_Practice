7a4b157cac3c349acf70f7f542e361a87ff0b15b
Mutatio02 <144579556+Mutatio02@users.noreply.github.com>
2025-02-25 23:18:39 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
@@ -1,33 +1,31 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <map>
+#include <bits/stdc++.h>
 using namespace std;
 
+// 25.02.25 패션왕 신예빈 복습 (해시)
+int T;
 
-int main(void) {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-    int T;
     cin >> T;
     while(T--) {
-        int n;
-        cin >> n;
-        map<string,int> mp; // 의상 종류 및 개수
-        for(int i=0; i<n; i++) {
-            string s1,s2; // 의상이름및 의상 종류
-            cin >> s1 >> s2;
-            mp[s2]++; // 의상 종류만 관심 있음
+        int N;
+        cin >> N;
+        unordered_map<string,int> mp;
+        while(N--) {
+            string name,kind;
+            cin >> name >> kind; // 종류만 신경 쓴다
+            mp[kind]++;
         }
 
-        int result = 1;
-        for(auto k : mp) { 
-            result *= (k.second+1); // 의상을 입지 않은경우 고려
+        int ans = 1;
+        for(auto it: mp) {
+            ans *= (it.second+1); // 각종류 + 안입는 경우
         }
-        result -= 1; // 둘다 안입은 경우제외
-        cout << result << '\n';
-
+        ans -=1; // 모두 안입는 경우(여사건)
+        cout << ans << "\n";
     }
+
     return 0;
 }
