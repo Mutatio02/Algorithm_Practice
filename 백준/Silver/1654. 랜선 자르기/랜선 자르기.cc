86a81cbd4f0dd925c34c9755dc448fe9f734ecf6
SungJun An <144579556+Mutatio02@users.noreply.github.com>
2025-03-25 19:28:51 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -1,45 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// 25.02.20 랜선 자르기 복습 (이분탐색) 
-int K,N;
-int line[10001];
+// 25.03.24 랜선 자르기 복습(이분 탐색)
 
+int K,N;
+int line[10005];
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
     cin >> K >> N;
-    int length = 0;
+    int limit = 0;
     for(int i=0; i<K; i++) {
         cin >> line[i];
-        length = max(length,line[i]);
+        limit = max(limit,line[i]);
     }
 
-    long long start = 1;
-    long long end = length;
-    long long result = 0;
+    long long left = 1;
+    long long right = limit;
+    long long ans = 0;
 
-    while(start<=end) {
-        long long mid = (start+end)/2;
+    while(left<=right) {
+        long long mid = (left + right)/2;
         int cnt = 0;
-        
+
         for(int i=0; i<K; i++) {
-            cnt += line[i]/mid; // mid로 나눴을떄
+            cnt += line[i]/mid;
         }
 
-        if(cnt>=N) { // 개수가 N개 이상
-            result = mid; // 값은 mid가 될 것이다
-            start = mid+1;
+        if(cnt>=N) {
+            ans = mid;
+            left = mid+1;
+        } 
+        else {
+            right = mid-1;
         }
-        else { // N개 미만
-            end = mid-1;
-        }   
-        
     }
 
-    cout << result;
-
+    cout << ans;
+    
     return 0;
 }
